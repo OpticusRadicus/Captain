cda1f3475900ffc928037f08df9601529e3d1e56
Deltron <jamiedbuell@gmail.com>
2014-03-23 23:29:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Captain::Application.config.secret_token = 'ea3b91cf418105872d7ce0c60f9316acd3472eb60ff6fa99337efcba9412bffab80c3da4cc6e68839027af1978f117cd22c9abfb0885f06fbdb9f8ed4bc65a3f'
